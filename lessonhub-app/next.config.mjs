6feaa6bcc05b04b90159b7141d1c4ba5fba6911c
Dunno Much <ve@theblifemovement.com>
2025-09-16 16:10:54 +0200

    hopefully footer now works correctly for the versioning

--- lessonhub-app/next.config.mjs
+++ lessonhub-app/next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
+  // expose the environment variable
+  env: {
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA,
+  },
 };
 
 export default nextConfig;
