6922742e63b8e9330e186080b739b5f3112b8358
Dunno Much <ve@theblifemovement.com>
2025-12-11 19:53:06 +0100

    more dark theme issues fixed

--- lessonhub-app/src/app/components/Providers.tsx
+++ lessonhub-app/src/app/components/Providers.tsx
@@ -5,6 +5,7 @@
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from 'next-themes';
 
 interface Props {
   children: ReactNode;
@@ -14,8 +15,15 @@ export default function Providers({ children }: Props) {
   // Refetch session every 5 minutes to reduce unnecessary database queries
   return (
     <SessionProvider refetchInterval={5 * 60}>
-      {children}
-      <Toaster position="top-right" richColors closeButton />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster position="top-right" richColors closeButton />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
